4d95a66b053d80a76082be57be419c1a76f13459
devinaresty <145647842+devinaresty@users.noreply.github.com>
2023-09-21 14:47:35 +0700

    Create perulangan.py

---
+++ perulangan.py
@@ -0,0 +1,18 @@
+i = 1
+while i <= 10: 
+ print(i)
+ i += 1
+
+f = 1
+while f <= 6: 
+ print(i) 
+ if f == 3:
+     break
+ f += 1
+
+k = 0
+while i < 6:
+    k += 1
+    if k == 3:
+        continue 
+    print(k)
