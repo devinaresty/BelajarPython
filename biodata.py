ff5353aa87df599fb7ce1366323f83e675ce0af3
devinaresty <145647842+devinaresty@users.noreply.github.com>
2023-09-21 14:50:43 +0700

    Create biodata.py

---
+++ biodata.py
@@ -0,0 +1,7 @@
+nama = input(" ")
+alamat = input(" ")
+tanggal = input(" ")
+umur = input(" ")
+
+print("hallo nama saya",nama,"alamat saya",alamat,"tanggal lahir saya",tanggal,"dan umur saya",umur)
+
